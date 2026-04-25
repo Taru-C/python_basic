5859eaaa76439e4f3097cfb2d3a131100275f89f
Taru Sojish Choudhari <taruchoudhari15@gmail.com>
2026-04-25 21:44:04 +0530

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,4 @@
+import random
+
+print("Rolling dice...")
+print("Result:", random.randint(1, 6))
---
+++ stopwatch.py
@@ -0,0 +1,9 @@
+import time
+
+input("Press Enter to start...")
+start = time.time()
+
+input("Press Enter to stop...")
+end = time.time()
+
+print("Time taken:", end - start, "seconds")
